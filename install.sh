644d898816cc5850c6e958a020e80a0121f6ac91
Franco Catena <francocatena@gmail.com>
2013-04-07 20:33:24 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+RBENV_DIR="/opt/rbenv"
+
+if [ ! -d "$RBENV_DIR" ]
+  git clone git://github.com/sstephenson/rbenv.git "$RBENV_DIR"
+  git clone git://github.com/sstephenson/ruby-build.git "$RBENV_DIR/plugins/ruby-build"
+fi
