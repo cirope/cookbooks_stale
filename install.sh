1bee77f5e776548aa92cc3f70030d78190d15eb0
Franco Catena <francocatena@gmail.com>
2013-04-07 20:36:57 -0300

    Ups, _little_ syntax mistake in install.sh =)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 RBENV_DIR="/opt/rbenv"
 
-if [ ! -d "$RBENV_DIR" ]
-  git clone git://github.com/sstephenson/rbenv.git "$RBENV_DIR"
-  git clone git://github.com/sstephenson/ruby-build.git "$RBENV_DIR/plugins/ruby-build"
+if [ ! -d "$RBENV_DIR" ]; then
+  git clone git://github.com/sstephenson/rbenv.git "$RBENV_DIR";
+  git clone git://github.com/sstephenson/ruby-build.git "$RBENV_DIR/plugins/ruby-build";
 fi
