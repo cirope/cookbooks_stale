b65b4bda129d4907a9671b124e9e8c472c962460
Franco Catena <francocatena@gmail.com>
2013-04-07 21:13:36 -0300

    Added chef installation and ruby check...

--- install.sh
+++ install.sh
@@ -10,4 +10,10 @@ if [ ! -d "$RBENV_ROOT" ]; then
   git clone git://github.com/sstephenson/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
 fi
 
-$RBENV_ROOT/bin/rbenv install $RUBY_VERSION
+if [ ! -d "$RBENV_ROOT/versions/$RUBY_VERSION" ]; then
+  $RBENV_ROOT/bin/rbenv install $RUBY_VERSION
+fi
+
+export PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH"
+
+gem install chef --no-ri --no-rdoc
