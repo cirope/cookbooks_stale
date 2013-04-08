39b3c570900645c4f3868ac88fab026361099ae3
Franco Catena <francocatena@gmail.com>
2013-04-07 23:36:43 -0300

    Link global rbenv to the user during install

--- install.sh
+++ install.sh
@@ -3,13 +3,16 @@
 export RBENV_ROOT="/opt/rbenv"
 RUBY_VERSION="2.0.0-p0"
 
+# Just install the minimal to compile ruby
 apt-get -y install build-essential libssl-dev git-core
 
+# Install rbenv
 if [ ! -d "$RBENV_ROOT" ]; then
   git clone git://github.com/sstephenson/rbenv.git "$RBENV_ROOT"
   git clone git://github.com/sstephenson/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
 fi
 
+# Install ruby
 if [ ! -d "$RBENV_ROOT/versions/$RUBY_VERSION" ]; then
   $RBENV_ROOT/bin/rbenv install $RUBY_VERSION
 fi
@@ -22,3 +25,11 @@ rbenv rehash
 gem install chef --no-ri --no-rdoc
 
 rbenv rehash
+
+# Link global rbenv to the current user
+if [ ! -d "$HOME/.rbenv" ]; then
+  ln -s $RBENV_ROOT "$HOME/.rbenv"
+
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
+  echo 'eval "$(rbenv init -)"' >> ~/.profile
+fi
