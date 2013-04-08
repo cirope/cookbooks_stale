8d1c032c2e6c8381af59837a24bc1984aba37134
Franco Catena <francocatena@gmail.com>
2013-04-08 08:14:54 -0300

    PostgreSQL install fix 2 =)

--- scripts/postgresql/install.sh
+++ scripts/postgresql/install.sh
@@ -5,7 +5,7 @@ PG_VERSION="9.1"
 if [ ! -d /etc/postgresql ]; then
   apt-get -y install postgresql libpq-dev
 
-  cp "$COOKBOOKS_DIR/files/postgresql/pg_hba.conf" "/etc/postgresql/$PG_VERSION/main/"
+  cp "$COOKBOOKS_DIR/files/config/postgresql/pg_hba.conf" "/etc/postgresql/$PG_VERSION/main/"
 
   service postgresql restart
 fi
