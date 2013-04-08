6e7eba800c37188d44fd53583c434de7eb6ffd73
Franco Catena <francocatena@gmail.com>
2013-04-08 19:10:43 -0300

    Another attempt of rbenv working on production...

--- scripts/rbenv/install.sh
+++ scripts/rbenv/install.sh
@@ -1,17 +1,24 @@
-export RBENV_ROOT="/opt/rbenv"
 RUBY_VERSION="2.0.0-p0"
 
 # Just install the minimal to compile ruby
 apt-get -y update
-apt-get -y install build-essential libssl-dev git-core
+apt-get -y install build-essential libssl-dev git-core curl
+
+DEPLOYER_HOME=/home/deployer
+RBENV_ROOT=$DEPLOYER_HOME/.rbenv
 
 # Install rbenv
-if [ ! -d "$RBENV_ROOT" ]; then
-  git clone git://github.com/sstephenson/rbenv.git "$RBENV_ROOT"
-  git clone git://github.com/sstephenson/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
-fi
+su deployer
+curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 
-# Install ruby
+echo "export RBENV_ROOT=\"\${HOME}/.rbenv\"" >> $DEPLOYER_HOME/.profile
+echo "if [ -d \"\${RBENV_ROOT}\" ]; then" >> $DEPLOYER_HOME/.profile
+echo "  export PATH=\"\${RBENV_ROOT}/bin:\${PATH}\"" >> $DEPLOYER_HOME/.profile
+echo "  eval \"\$(rbenv init -)\"" >> $DEPLOYER_HOME/.profile
+echo "fi" >> $DEPLOYER_HOME/.profile
+
+
+ #Install ruby
 if [ ! -d "$RBENV_ROOT/versions/$RUBY_VERSION" ]; then
   $RBENV_ROOT/bin/rbenv install $RUBY_VERSION
 fi
@@ -21,14 +28,4 @@ export PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH"
 rbenv global $RUBY_VERSION
 rbenv rehash
 
-# Link global rbenv to the current user
-if [ ! -d "$HOME/.rbenv" ]; then
-  ln -s $RBENV_ROOT "$HOME/.rbenv"
-
-  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-  echo 'eval "$(rbenv init -)"' >> ~/.profile
-
-  exec $SHELL -l
-
-  cp files/users/.gemrc $HOME
-fi
+sudo su -
