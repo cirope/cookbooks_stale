f46efe18d80327b88bd2c28ab710e0411eb9cb3f
Franco Catena <francocatena@gmail.com>
2013-04-08 21:18:58 -0300

    Reload rbenv environment...

--- scripts/rbenv/deployer_install.sh
+++ scripts/rbenv/deployer_install.sh
@@ -6,19 +6,12 @@ RBENV_ROOT=$DEPLOYER_HOME/.rbenv
 # Install rbenv
 curl https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
 
-echo "export RBENV_ROOT=\"\${HOME}/.rbenv\"" >> $DEPLOYER_HOME/.profile
-echo "if [ -d \"\${RBENV_ROOT}\" ]; then" >> $DEPLOYER_HOME/.profile
-echo "  export PATH=\"\${RBENV_ROOT}/bin:\${PATH}\"" >> $DEPLOYER_HOME/.profile
-echo "  eval \"\$(rbenv init -)\"" >> $DEPLOYER_HOME/.profile
-echo "fi" >> $DEPLOYER_HOME/.profile
-
+exec $SHELL -l
 
 # Install ruby
 if [ ! -d "$RBENV_ROOT/versions/$RUBY_VERSION" ]; then
-  $RBENV_ROOT/bin/rbenv install $RUBY_VERSION
+  rbenv install $RUBY_VERSION
 fi
-
-export PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH"
 
 rbenv global $RUBY_VERSION
 rbenv rehash
--- scripts/rbenv/install.sh
+++ scripts/rbenv/install.sh
@@ -4,8 +4,12 @@ RUBY_VERSION="2.0.0-p0"
 apt-get -y update
 apt-get -y install build-essential libssl-dev git-core curl
 
+if [ ! -d /home/deployer/.rbenv ]; then
+  cat files/config/rbenv/env.sh >> /home/deployer/.profile
+fi
+
 cp scripts/rbenv/deployer_install.sh /home/deployer/
-chmod 777 /home/deployer/deployer_install.sh
+chown deployer:www-data /home/deployer/deployer_install.sh
 
 su -l deployer -c /home/deployer/deployer_install.sh
 rm /home/deployer/deployer_install.sh
